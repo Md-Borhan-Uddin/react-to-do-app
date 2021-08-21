3a93ac0212e80e01edf0b42ae30afa2d452cf1ce
Md-Borhan-Uddin <mdborhanuddinab21@gmail.com>
2021-08-21 19:43:46 +0600

    delete unnecessary

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with google
         </a>
       </header>
     </div>
